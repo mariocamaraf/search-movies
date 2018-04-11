1c9882138ca019c98576b32d05194810db2fa106
Mario Cámara <macf@MacBook-Pro-de-Mario.local>
2018-04-11 13:21:52 +0200

    adding search movies app

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+
+import './App.css';
+import 'bulma/css/bulma.css'
+
+class App extends Component { 
+  render() {
+
+    return (
+      <div className="App">
+       <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/detail/:movieId' component={Detail} />
+       </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Detail.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const API_KEY = 'bf7025b7'
+
+export class Detail extends Component {
+    static propTypes={
+        match: PropTypes.shape({
+            params: PropTypes.object,
+            isExact: PropTypes.bool,
+            path: PropTypes.string,
+            url: PropTypes.string,
+        })
+    }
+    state = { movie: {} }
+
+_fetchMovie ({ id }){
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+    .then(res => res.json())
+    .then(movie => {
+       console.log({movie})
+       this.setState({ movie })
+    })  
+}
+    
+componentDidMount(){
+    console.log(this.props)
+    const {movieId} = this.props.match.params
+    this._fetchMovie({ id: movieId })
+}
+    _goback(){
+    window.history.back()
+}
+
+    render() {
+        const {Title, Poster, Actors, Metascore, Plot} =
+        this.state.movie
+
+        return(
+        <div>
+            <button onClick={this._goback}> Volver</button>
+            <h1>{Title}</h1>
+            <img src={Poster} />
+            <h3>{Actors}</h3>
+            <span>{Metascore}</span>
+            <p>Plot</p>
+            </div>
+        )
+    }
+}
